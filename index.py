a51b96f4cc0f35e38d87f833d9c6a5a339135402
Jamil Sostizzo <jamil.sostizzo@gmail.com>
2025-02-11 15:39:24 +0100

    liste ist kapput

--- index.py
+++ index.py
@@ -30,14 +30,14 @@ BLACK = (0,0,0)
 PATH_COLOR = (150, 150, 150)
 
 # Pfaddefinition
-PATH = [
-    (0, 6), (1, 6), (2, 6), (3, 6), (4, 6),
+PATH = {"easy":
+    [(0, 6), (1, 6), (2, 6), (3, 6), (4, 6),
     (5, 6), (5, 7), (5, 8), (4, 8), (3, 8),
     (2, 8), (2, 7), (2, 6), (2, 5), (2, 4), 
     (2, 3), (3, 3), (4, 3), (5, 3), (6, 3),
     (7, 3), (8, 3), (8, 4), (8, 5), (8, 6),
-    (8, 7), (8, 8), (8, 9)
-]
+    (8, 7), (8, 8), (8, 9)]
+}
 
 # Tower-Typen mit Preisen und Eigenschaften
 TOWER_TYPES = [
@@ -48,7 +48,7 @@ TOWER_TYPES = [
 
 class Enemy:
     def __init__(self):
-        self.path = deque(PATH)
+        self.path = deque(PATH["easy"])
         self.pos = pygame.Vector2(self.path[0][0] * CELL_SIZE + CELL_SIZE // 2, 
                                   self.path[0][1] * CELL_SIZE + CELL_SIZE // 2)
         self.target_index = 1
